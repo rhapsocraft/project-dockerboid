37645e66e140e0c78824b3a981e03d765992520b
Sean <axciom545@gmail.com>
2026-04-09 22:38:34 +0800

    config adjustments

--- start.sh
+++ start.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-# Create a symlink
-# RV Interior Map
-RV_INTERIOR_MAP_SOURCE="./links/maps/"
-RV_INTERIOR_MAP_NAME="map_distanciado"
-RV_INTERIOR_MAP_TARGET="./workshop-mods/content/108600/3543229299/mods/modPROJECTRVInterior/common/media/maps/$RV_INTERIOR_MAP_NAME/"
-
-if ! [ -d "$RV_INTERIOR_MAP_SOURCE/$RV_INTERIOR_MAP_NAME" ]; then
-    # Create link if map doesn't not exist
-    echo "Creating RV_INTERIOR map symlink"
-    ln -s "$RV_INTERIOR_MAP_TARGET" "$RV_INTERIOR_MAP_SOURCE"
-fi
-
+docker compose up
