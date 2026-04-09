2301daf41ac00376120e9e981bf6b4aa59dfb4fb
Sean <axciom545@gmail.com>
2026-04-09 22:06:05 +0800

    update line-endings

--- start.sh
+++ start.sh
@@ -2,12 +2,13 @@
 
 # Create a symlink
 # RV Interior Map
-RV_INTERIOR_MAP_SOURCE="./links/maps"
-RV_INTERIOR_MAP_MAP_NAME="map_distanciado"
-RV_INTERIOR_MAP_TARGET="./workshop-mods/content/108600/3543229299/mods/modPROJECTRVInterior/common/media/maps/$RV_INTERIOR_MAP_MAP_NAME/"
+RV_INTERIOR_MAP_SOURCE="./links/maps/"
+RV_INTERIOR_MAP_NAME="map_distanciado"
+RV_INTERIOR_MAP_TARGET="./workshop-mods/content/108600/3543229299/mods/modPROJECTRVInterior/common/media/maps/$RV_INTERIOR_MAP_NAME/"
 
-if ! [ -d "$RV_INTERIOR_MAP_SOURCE/$RV_INTERIOR_MAP_MAP_NAME" ]; then
+if ! [ -d "$RV_INTERIOR_MAP_SOURCE/$RV_INTERIOR_MAP_NAME" ]; then
     # Create link if map doesn't not exist
-    ln -s "$TARGET" "$SOURCE"
+    echo "Creating RV_INTERIOR map symlink"
+    ln -s "$RV_INTERIOR_MAP_TARGET" "$RV_INTERIOR_MAP_SOURCE"
 fi
 
